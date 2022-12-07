18c150e09a51d6653826eb35336cb26d972426a0
Toddynn <viniciusgabriez07@gmail.com>
2022-12-07 08:39:22 -0300

    Update auth.js

--- sistema/src/contexts/auth.js
+++ sistema/src/contexts/auth.js
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import firebase from '../services/fireBaseConnection';
 
-export const AuthContext = createContext({});
+export const  AuthContext = createContext({});
 
 function AuthProvider({children}){
     const [user, setUser] = useState(null);
